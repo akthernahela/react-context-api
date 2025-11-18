e9cb4a5dde14dd0a8acfe04e617b4674d361b880
akthernahela <nahela00@gmail.com>
2025-11-18 21:09:24 +0100

    Add the use context

--- src/shop/Products.jsx
+++ src/shop/Products.jsx
@@ -3,7 +3,7 @@ import Main from "../components/Main";
 import Footer from "../components/Footer";
 import { Link } from 'react-router-dom';
 import Budget from "../components/Budget";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 
 export default function Products() {
     const products = [
