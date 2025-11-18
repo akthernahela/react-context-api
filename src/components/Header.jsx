b5cf5f0643061c807df175c61332f47d22e3d7d5
akthernahela <nahela00@gmail.com>
2025-11-18 19:41:50 +0100

    Create a variable to include use context inside it

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import Budget from "./Budget";
 
 export default function Header() {
+    const { budgetMode, setBudgetMode } = useContext();
 
     return (
         <>
