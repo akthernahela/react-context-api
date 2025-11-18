a0d06ada0d1ba374a01631e0bb77145d950a265a
akthernahela <nahela00@gmail.com>
2025-11-18 21:03:20 +0100

    Import budget to the products

--- src/shop/Products.jsx
+++ src/shop/Products.jsx
@@ -2,6 +2,7 @@ import Header from "../components/Header";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
 import { Link } from 'react-router-dom';
+import Budget from "../components/Budget";
 
 export default function Products() {
     const products = [
