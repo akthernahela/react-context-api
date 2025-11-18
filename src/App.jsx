7bb732ca9baed450973f7f8ddfa7bcc8e907450c
akthernahela <nahela00@gmail.com>
2025-11-18 20:51:55 +0100

    Import the function

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import HomePage from "./shop/HomePage";
 import AboutUs from "./shop/AboutUs";
 import Products from "./shop/Products";
 import SingleProduct from './shop/SingleProduct';
-import Budget from "./components/Budget";
+import { BudgetProvider } from "./components/Budget";
 
 function App() {
 
 
   return (
     <>
-      <Budget.Provider value={{ budgetMode, setBudgetMode }}>
+      <Budget.Provider>
         <BrowserRouter>
           <Routes>
             <Route path='/' element={<HomePage />} />
--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -1,3 +1,12 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 const Budget = createContext()
+export function BudgetProvider({ children }) {
+    const [budgetMode, setBudgetMode] = useState(false);
+
+    return (
+        <Budget.Provider value={{ budgetMode, setBudgetMode }}>
+            {children}
+        </Budget.Provider>
+    );
+}
 export default Budget
